092aa716091998fcbf693d4aff95c3e4474d43e7
Xavier Mejía <xavierams08@gmail.com>
2021-09-26 23:02:42 -0400

    first commit

---
+++ src/app/interfaces/student.interface.ts
@@ -0,0 +1,15 @@
+// export interface Welcome {
+//     students: Student[];
+// }
+
+export interface Student {
+    city:      string;
+    company:   string;
+    email:     string;
+    firstName: string;
+    grades:    string[];
+    id:        string;
+    lastName:  string;
+    pic:       string;
+    skill:     string;
+}
